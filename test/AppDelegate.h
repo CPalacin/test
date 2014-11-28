3d3f4cb2293fc560fa0e8250d194b40c0f0f978a
Carlos Palacin <palacin38@hotmail.com>
2014-11-27 22:03:49 -0800

    test

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Carlos Rubio on 11/27/14.
+//  Copyright (c) 2014 Carlos Rubio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  test
+//
+//  Created by Carlos Rubio on 11/27/14.
+//  Copyright (c) 2014 Carlos Rubio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
